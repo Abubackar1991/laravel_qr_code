912df95fd32af42a5d9456934773b3b87649ed8e
Abubackar1991 <66620297+Abubackar1991@users.noreply.github.com>
2023-05-10 17:40:33 +0700

    QR Name

--- resources/views/qrcode.blade.php
+++ resources/views/qrcode.blade.php
@@ -13,7 +13,7 @@
                 <h2>Simple QR Code</h2>
             </div>
             <div class="card-body">
-                {!! QrCode::size(300)->generate('RemoteStack') !!}
+                {!! QrCode::size(300)->generate('Abubackar Sidique') !!}
             </div>
         </div>
         <div class="card">
@@ -21,7 +21,7 @@
                 <h2>Color QR Code</h2>
             </div>
             <div class="card-body">
-                {!! QrCode::size(300)->backgroundColor(255,90,0)->generate('RemoteStack') !!}
+                {!! QrCode::size(300)->backgroundColor(255,90,0)->generate('Abubackar Sidique') !!}
             </div>
         </div>
     </div>
